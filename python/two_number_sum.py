e531cfef459f408ce8b42e1867e43915b9019cbc
Simit Tomar <simittomar@Simits-MacBook-Pro.local>
2022-01-29 02:36:15 +0000

    added two num sum

--- python/two_number_sum.py
+++ python/two_number_sum.py
@@ -1,18 +1,18 @@
 # num_list = [3, 5, -4, 8, 11, -1, 1, 6]
 
-sorted = [ -1, 2, 3, 5, 6, 8, 9, 10]
+sorted_num_list = [ -1, 2, 3, 5, 6, 8, 9, 10]
 target_sum = 11
 
-# def two_number_sum(num_list, target_sum):
-#     list_len = len(num_list) -1
-#     for i in range(list_len):
-#         first_num = num_list[i]
-#         second_num = target_sum - num_list[i+1]
-#         if second_num in num_list:
-#             return [first_num, second_num]
+def two_number_sum(num_list, target_sum):
+    list_len = len(num_list) -1
+    for i in range(list_len):
+        first_num = num_list[i]
+        second_num = target_sum - num_list[i+1]
+        if second_num in num_list:
+            return [first_num, second_num]
     
-#     return []
+    return []
 
 
-# res_list = two_number_sum(num_list, target_sum)
-# print('res_list', res_list)
+res_list = two_number_sum(sorted_num_list, target_sum)
+print('res_list', res_list)
